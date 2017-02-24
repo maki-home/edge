e72e96118d556b6d0bf0bfe850ca86fe0dec1932
making <makingx@gmail.com>
2017-02-25 01:05:45 +0900

    first commit

---
+++ src/main/java/ik/am/EdgeApplication.java
@@ -0,0 +1,14 @@
+package ik.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class EdgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeApplication.class, args);
+	}
+}
